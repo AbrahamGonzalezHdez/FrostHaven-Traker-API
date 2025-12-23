da1b396637f666cf331f9522e336883c631f47a1
aGonzalezHdez <agonzalez.mex@grupoei.com.mx>
2025-12-23 16:23:06 -0600

    feat: Configuracion inicial

---
+++ FrostHaven-Traker-API/APIs/Controllers/UsuarioController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace APIs.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class UsuarioController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult GetUsuarios()
+    {
+        var usuarios = new[]
+        {
+            new
+            {
+                Id = 1,
+                Nombre = "Administrador",
+                Email = "admin@demo.com",
+                Activo = true
+            }
+        };
+
+        return Ok(usuarios);
+    }
+}
